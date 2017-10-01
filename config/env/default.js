d5c93dc25b3319f0f35cb9192a2ed52e963567fb
Kyle Walsh <kylegwalsh@ufl.edu>
2017-10-01 15:04:43 -0400

    Modified default settings, added favicon, created header, created footer, created landing page

--- config/env/default.js
+++ config/env/default.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    title: 'Human Centered Computing Research Portal',
+    description: 'Search for opportunities to participate in research studies hosted by the University of Florida\'s Human Centered Computing researchers.',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
