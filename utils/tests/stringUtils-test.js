39d938ae3acddd15ee1dd7115b2e00a98b42715c
Trenton w Fleming <trenflem@gmail.com>
2017-10-30 04:27:34 -0400

    added test for sting utils

--- utils/tests/stringUtils-test.js
+++ utils/tests/stringUtils-test.js
@@ -1,3 +1,9 @@
-/**
- * Created by trentonfleming on 10/30/17.
- */
+const stringUtils = require('../stringUtils');
+const assert = require('assert');
+
+describe('GIVEN String, RETURN STRING IN TITLE CASE', () => {
+  it(`GIVEN 'ufHcc Is tHe best Ever.', RETURN 'Ufhcc Is The Best Ever.'`, () => {
+    const ugly = 'ufHcc Is tHe best Ever.';
+    assert.equal(stringUtils.toTitleCase(ugly), 'Ufhcc Is The Best Ever.');
+  });
+});
