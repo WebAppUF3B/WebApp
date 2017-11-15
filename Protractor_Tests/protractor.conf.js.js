f2d90c5205c1c5db2f8575e17dd9ff882473dd8c
Christopher Kwan <ckwan@ufl.edu>
2017-11-15 18:56:14 -0500

    Protractor Spec Fix. Rollback to V 2.5.1 w/ Jasmine Spec Reporter V 4.2.1

--- Protractor_Tests/protractor.conf.js.js
+++ Protractor_Tests/protractor.conf.js.js
@@ -1,4 +1,7 @@
 // An example configuration file.
+
+
+
 exports.config = {
   directConnect: true,
 
@@ -9,13 +12,26 @@ exports.config = {
 
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    isVerbose: true,
+    print: function () {} // remove dots for each test
+  },
+
+  onPrepare: function () {
+    const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    jasmine.getEnv().addReporter(new SpecReporter());
+},
+
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['example_spec.js'],
+  specs: ['dummytest_spec.js','test.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   }
+
+
 };
