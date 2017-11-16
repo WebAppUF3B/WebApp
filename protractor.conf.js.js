c5e78739bd0d5fb2ce0fd50c4a47cb0b715e80e9
Christopher Kwan <ckwan@ufl.edu>
2017-11-16 13:08:17 -0500

    Revert "Sign e2e testing"
    This reverts commit 981f30a047aaa15c7b5e0859c7a71c5b2606a2fa.

--- protractor.conf.js.js
+++ protractor.conf.js.js
@@ -1,7 +1,4 @@
 // An example configuration file.
-
-
-
 exports.config = {
   directConnect: true,
 
@@ -12,26 +9,13 @@ exports.config = {
 
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    isVerbose: true,
-    print: function () {} // remove dots for each test
-  },
-
-  onPrepare: function () {
-    const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
-    jasmine.getEnv().addReporter(new SpecReporter());
-},
-
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['dummytest_spec.js','test.js'],
+  specs: ['dummytest.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   }
-
-
 };
